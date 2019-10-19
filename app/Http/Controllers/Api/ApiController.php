3f1b1d46084cd7dd06fc8407eb1f00d40dfd1987
Ümit UZ <umituz998@gmail.com>
2019-10-19 19:19:55 +0300

    ApiEnumeration file was created

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enumerations\ApiEnumeration;
 use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
 {
-    public function apiResponse($data, $message = null, $code = 200)
+    public function apiResponse($data, $message = null, $code = 200, $status = ApiEnumeration::SUCCESS)
     {
-        $response = [
-            'code' => $code,
-            'message' => $message,
-            'data' => $data
-        ];
+        $response = array();
+
+        $response['success'] = $status == ApiEnumeration::SUCCESS ? true : false;
+
+        if ($status != ApiEnumeration::ERROR) {
+            $response['data'] = $data;
+        }
+
+        if ($status == ApiEnumeration::ERROR) {
+            $response['errors'] = $data;
+        }
+
+        if (isset($message) && !is_null($message)) {
+            $response['message'] = $message;
+        }
 
         return response()->json($response, $code);
     }
