c2f6a12e2084ae40b78a4f009c3525635d903727
Ümit UZ <umituz998@gmail.com>
2019-10-19 16:05:55 +0300

    User model $casts for created_at and updated_at

--- app/User.php
+++ app/User.php
@@ -35,6 +35,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'created_at' => 'date:Y-m-d',
+        'updated_at' => 'date:Y-m-d H:i:s'
     ];
 
 //    protected $appends = ['full_name'];
