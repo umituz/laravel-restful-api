5cc95b177c64ffcbb07a16ac9b092cd4047e6890
Ümit UZ <umituz998@gmail.com>
2019-11-16 00:35:44 +0300

    basic authentication

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth.basic')->get('/user-basic', function (Request $request) {
+    return $request->user();
+});
+
 //Route::get('/merhaba',function(){
 //    return "hello";
 //});
